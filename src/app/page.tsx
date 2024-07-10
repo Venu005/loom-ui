98581ca7340559eba6328f44a1b8c0fab04dd409
Venu005 <venusaiyalamanchili@gmail.com>
2024-07-10 15:27:43 +0530

    chore:change file structure from components to components/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/Button";
+import { Button } from "@/components/ui/Button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button variant={"destructive"}>Hello</Button>
+      <Button>Hello</Button>
     </main>
   );
 }
